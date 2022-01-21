9b900e37271547c97935326127ae45a5608bf769
Anastasiia000 <nahornyatko.17@gmail.com>
2022-01-21 21:56:05 +0100

    publisher

--- server chat/Form1.Designer.cs
+++ server chat/Form1.Designer.cs
@@ -24,13 +24,71 @@ namespace server_chat {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelMessenger = new System.Windows.Forms.Label();
+            this.txtWrite = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtWrite);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelMessenger
+            // 
+            this.labelMessenger.AutoSize = true;
+            this.labelMessenger.Location = new System.Drawing.Point(503, 42);
+            this.labelMessenger.Name = "labelMessenger";
+            this.labelMessenger.Size = new System.Drawing.Size(50, 20);
+            this.labelMessenger.TabIndex = 0;
+            this.labelMessenger.Text = "label1";
+            // 
+            // txtWrite
+            // 
+            this.txtWrite.Location = new System.Drawing.Point(11, 314);
+            this.txtWrite.Multiline = true;
+            this.txtWrite.Name = "txtWrite";
+            this.txtWrite.Size = new System.Drawing.Size(259, 101);
+            this.txtWrite.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(288, 326);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(66, 101);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelMessenger);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelMessenger;
+        private System.Windows.Forms.TextBox txtWrite;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
--- server chat/Form1.cs
+++ server chat/Form1.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,5 +16,25 @@ namespace server_chat {
             InitializeComponent();
         }
 
+        private void btnSend_Click(object sender, EventArgs e) {
+            int timeToSleep = new Random().Next(1000, 3000);
+            Thread.Sleep(timeToSleep);
+            var factory = new ConnectionFactory() { HostName = "localhost" }; //создаем инстантс для соединения сервера //работаем с очередью на локальной машине
+            using (var connection = factory.CreateConnection()) //содаем абстракциюб которая будет управлять версией протоколаб унификацией и тд
+            using (var channel = connection.CreateModel()) {
+                //объявляем очередь, куда мы будем публиковать наше сообщения
+                channel.QueueDeclare(queue: "dev-queue",
+                    durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                //переводим сообщение, так как сигнатура метода требует байты
+                var body = Encoding.UTF8.GetBytes(txtWrite.Text);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "dev-queue", basicProperties: null, body: body);
+
+                labelMessenger.Text +="\n"+ txtWrite.Text;
+                txtWrite.Clear();
+            }
+        }
     }
 }
